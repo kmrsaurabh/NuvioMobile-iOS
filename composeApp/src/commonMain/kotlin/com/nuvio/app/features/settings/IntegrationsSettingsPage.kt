ff22295922257ff2780bb464b28087a537abbc66
Luqman Fadlli <75630095+luqmanfadlli@users.noreply.github.com>
2026-05-23 17:48:53 +0700

    feat: add Trakt to Integrations settings page

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/settings/IntegrationsSettingsPage.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/settings/IntegrationsSettingsPage.kt
@@ -3,18 +3,21 @@ package com.nuvio.app.features.settings
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.CloudQueue
 import androidx.compose.foundation.lazy.LazyListScope
-import nuvio.composeapp.generated.resources.compose_settings_page_debrid
 import nuvio.composeapp.generated.resources.Res
+import nuvio.composeapp.generated.resources.compose_settings_page_debrid
 import nuvio.composeapp.generated.resources.compose_settings_page_mdblist_ratings
 import nuvio.composeapp.generated.resources.compose_settings_page_tmdb_enrichment
-import nuvio.composeapp.generated.resources.settings_integrations_mdblist_description
+import nuvio.composeapp.generated.resources.compose_settings_page_trakt
+import nuvio.composeapp.generated.resources.compose_settings_root_trakt_description
 import nuvio.composeapp.generated.resources.settings_integrations_debrid_description
+import nuvio.composeapp.generated.resources.settings_integrations_mdblist_description
 import nuvio.composeapp.generated.resources.settings_integrations_section_title
 import nuvio.composeapp.generated.resources.settings_integrations_tmdb_description
 import org.jetbrains.compose.resources.stringResource
 
 internal fun LazyListScope.integrationsContent(
     isTablet: Boolean,
+    onTraktClick: () -> Unit,
     onTmdbClick: () -> Unit,
     onMdbListClick: () -> Unit,
     onDebridClick: () -> Unit,
@@ -25,6 +28,13 @@ internal fun LazyListScope.integrationsContent(
             isTablet = isTablet,
         ) {
             SettingsGroup(isTablet = isTablet) {
+                SettingsNavigationRow(
+                    title = stringResource(Res.string.compose_settings_page_trakt),
+                    description = stringResource(Res.string.compose_settings_root_trakt_description),
+                    iconPainter = integrationLogoPainter(IntegrationLogo.Trakt),
+                    isTablet = isTablet,
+                    onClick = onTraktClick,
+                )
                 SettingsNavigationRow(
                     title = stringResource(Res.string.compose_settings_page_tmdb_enrichment),
                     description = stringResource(Res.string.settings_integrations_tmdb_description),
