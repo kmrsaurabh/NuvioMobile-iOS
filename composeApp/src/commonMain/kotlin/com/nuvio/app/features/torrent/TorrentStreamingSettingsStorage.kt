affd5ca43ee5880f94fbe0c3d88896aae3de4239
kmrsaurabh <kmrsaurabh907@gmail.com>
2026-06-05 09:43:51 +0530

    fix: remove loadCustomTrackers from commonMain settings storage to fix iOS build

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/torrent/TorrentStreamingSettingsStorage.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/torrent/TorrentStreamingSettingsStorage.kt
@@ -15,8 +15,6 @@ expect object TorrentStreamingSettingsStorage {
     fun saveEnableUpnp(enable: Boolean)
     fun loadForceTcp(): Boolean?
     fun saveForceTcp(force: Boolean)
-    fun loadCustomTrackers(): String?
-    fun saveCustomTrackers(trackers: String)
     fun loadPreferredPort(): Int?
     fun savePreferredPort(port: Int)
     fun loadBatterySaver(): Boolean?
