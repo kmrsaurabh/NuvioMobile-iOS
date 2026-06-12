e78a651db16b19e2ebb8ea9979e4e77feee2f1a0
kmrsaurabh <kmrsaurabh907@gmail.com>
2026-06-12 13:38:00 +0530

    fix: use OTHER_LDFLAGS to link libtorrent instead of file references to avoid xcodeproj gem crash on Xcode 16 synchronized groups

--- iosApp/update_project.rb
+++ iosApp/update_project.rb
@@ -28,38 +28,8 @@ end
 
 # 2. Add C++ Engine Files (skipped because Xcode 16 Synchronized Groups will pick them up automatically)
 
-# 3. Link LibTorrent static libs (.a) and libc++.tbd
-Dir.glob('Frameworks/LibTorrent/lib/*.a').each do |lib_path|
-  # Skip vcpkg's iconv/charset because they shadow Apple's system libiconv.tbd,
-  # causing missing _iconv symbols for MPVKit/Libass.
-  next if lib_path.end_with?('libiconv.a') || lib_path.end_with?('libcharset.a')
-
-  lib_ref = project.new_file(lib_path)
-  target.frameworks_build_phase.add_file_reference(lib_ref)
-end
-
-
-# Add libc++.tbd for C++ standard library
-libcxx = project.new_file('usr/lib/libc++.tbd')
-libcxx.source_tree = 'SDKROOT'
-target.frameworks_build_phase.add_file_reference(libcxx)
-
-# Add libiconv.tbd (required by MPVKit/Libass since GoTorrent was removed)
-libiconv = project.new_file('usr/lib/libiconv.tbd')
-libiconv.source_tree = 'SDKROOT'
-target.frameworks_build_phase.add_file_reference(libiconv)
-
-
-# Add System frameworks required by libtorrent on iOS
-%w[
-  System/Library/Frameworks/SystemConfiguration.framework
-  System/Library/Frameworks/Security.framework
-  System/Library/Frameworks/CoreFoundation.framework
-].each do |fw|
-  fw_ref = project.new_file(fw)
-  fw_ref.source_tree = 'SDKROOT'
-  target.frameworks_build_phase.add_file_reference(fw_ref)
-end
+# 3. Link LibTorrent static libs and system frameworks via Build Settings
+# Avoid project.new_file because xcodeproj gem crashes on PBXFileSystemSynchronizedRootGroup
 
 # 4. Update Build Settings
 target.build_configurations.each do |config|
@@ -73,11 +43,29 @@ target.build_configurations.each do |config|
   lib_paths << '$(SRCROOT)/Frameworks/LibTorrent/lib'
   config.build_settings['LIBRARY_SEARCH_PATHS'] = lib_paths.uniq
   
-  # For ObjC++
+  # For ObjC++ and linking C++ static libraries
   config.build_settings['ENABLE_STRICT_OBJC_MSGSEND'] = 'YES'
   config.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
   config.build_settings['CLANG_CXX_LIBRARY'] = 'libc++'
-  config.build_settings['OTHER_LDFLAGS'] = ['$(inherited)', '-ObjC']
+  
+  ldflags = config.build_settings['OTHER_LDFLAGS'] || ['$(inherited)']
+  ldflags += [
+    '-ObjC',
+    '-ltorrent-rasterbar',
+    '-lboost_chrono',
+    '-lboost_container',
+    '-lboost_context',
+    '-lboost_date_time',
+    '-lboost_random',
+    '-lcrypto',
+    '-lssl',
+    '-lc++',
+    '-liconv',
+    '-framework', 'SystemConfiguration',
+    '-framework', 'Security',
+    '-framework', 'CoreFoundation'
+  ]
+  config.build_settings['OTHER_LDFLAGS'] = ldflags.uniq
   
   # Crucial for ABI compatibility with vcpkg libtorrent 2.0.11 compilation
   config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] = [
